77d5befde78dbd8339f73ad588ef0aaefb35fc3b
Vitalii Skrynkovych <vitaliiskrynkovych@gmail.com>
2021-04-19 20:19:00 +0300

    created StopWatch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import Stopwatch from './Stopwatch/Stopwatch';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Stopwatch</h2>
+      <Stopwatch />
     </div>
   );
 }
-
 export default App;
