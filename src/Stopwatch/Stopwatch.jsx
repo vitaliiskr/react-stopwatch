06781764c4c042faf92515ad2dc71e874fcf1c2c
Vitalii Skrynkovych <vitaliiskrynkovych@gmail.com>
2021-04-21 00:43:47 +0300

    made by using Rx.js

--- src/Stopwatch/Stopwatch.jsx
+++ src/Stopwatch/Stopwatch.jsx
@@ -1,177 +1,68 @@
 import React, { useEffect, useState } from 'react';
 import './Stopwatch.scss';
 import { interval, Subject } from 'rxjs';
-import {take} from 'rxjs/operators';
-
-// const Stopwatch = () => {
-//   const [isTimerStarted, setIsTimerStarted] = useState(false);
-//   const [timerId, setTimerId] = useState(null);
-//   const [seconds, setSeconds] = useState(0);
-//   const [minutes, setMinutes] = useState(0);
-//   const [hours, setHours] = useState(0);
-//   const [clickCount, setCount] = useState(0);
-
-//   // const subscription = interval(1000);
-
-//   const start = () => {
-//     setIsTimerStarted(true);
-//     setTimerId(setInterval(() => {
-//       setSeconds((prevSeconds) => prevSeconds + 1);
-//     }, 1000));
-//     // subscription.subscribe(() => {
-//     //   setSeconds((prevSeconds) => prevSeconds + 1);
-//     // });
-//   };
-
-//   const wait = () => {
-//     setIsTimerStarted(false);
-//     setTimerId(clearInterval(timerId));
-//   };
-
-//   const reset = () => {
-//     setSeconds(0);
-//     setMinutes(0);
-//     setHours(0);
-//   };
-
-//   const stop = () => {
-//     setIsTimerStarted(false);
-//     setTimerId(clearInterval(timerId));
-//     reset();
-//   };
-
-//   const startCount = () => {
-//     setCount((prevcount) => prevcount + 1);
-//     if (clickCount === 1) {
-//       wait();
-//     } else {
-//       setTimeout(() => {
-//         setCount(0);
-//       }, 300);
-//     }
-//   };
-
-//   useEffect(() => {
-//     if (seconds === 60) {
-//       setMinutes((prevMinutes) => prevMinutes + 1);
-//       setSeconds(0);
-//     }
-//     if (minutes === 60) {
-//       setHours((prevHours) => prevHours + 1);
-//       setMinutes(0);
-//     }
-//     if (hours === 24) {
-//       stop();
-//     }
-//   }, [seconds, minutes, hours]);
-
-//   return (
-//     <div className="stopwatch">
-
-//       <div className="stopwatch__screen">
-//         <div className="stopwatch__item">
-//           {hours < 10 ? `0${hours}` : hours}
-//         </div>
-//         <div className="stopwatch__border">:</div>
-//         <div className="stopwatch__item">
-//           {minutes < 10 ? `0${minutes}` : minutes}
-//         </div>
-//         <div className="stopwatch__border">:</div>
-//         <div className="stopwatch__item">
-//           {seconds < 10 ? `0${seconds}` : seconds}
-//         </div>
-//       </div>
-
-//       <div className="stopwatch__buttons">
-//         <button
-//           type="button"
-//           className="stopwatch__button"
-//           onClick={start}
-//           disabled={isTimerStarted}
-//         >
-//           Start
-//         </button>
-//         <button
-//           type="button"
-//           className="stopwatch__button"
-//           onClick={startCount}
-//         >
-//           Wait
-//         </button>
-//         <button
-//           type="button"
-//           className="stopwatch__button"
-//           onClick={stop}
-//         >
-//           Stop
-//         </button>
-//         <button
-//           type="button"
-//           className="stopwatch__button"
-//           onClick={reset}
-//         >
-//           Reset
-//         </button>
-//       </div>
-
-//     </div>
-//   );
-// };
-
-// export default Stopwatch;
+import { takeUntil } from 'rxjs/operators';
 
 const Stopwatch = () => {
-  const [isTimerStarted, setIsTimerStarted] = useState(false);
-  const [seconds, setSeconds] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [clickCount, setCount] = useState(0);
+  const [time, setTime] = useState(0);
+  const [isTimeStarted, setIsTimeStarted] = useState(false);
 
+  useEffect(() => {
+    const stream$ = new Subject();
+    interval(1000)
+      .pipe(
+        takeUntil(stream$)
+      )
+      .subscribe(() => {
+        if (isTimeStarted) {
+          setTime((prevTime) => prevTime + 1);
+        }
+      });
 
-  const stream$ = new Subject()
-  interval(1000)
-    
+    return () => {
+      stream$.next();
+      stream$.complete();
+    };
+  }, [isTimeStarted]);
 
   const start = () => {
-    setIsTimerStarted(true)
-    stream$.subscribe(() => setSeconds(prevSeconds => prevSeconds + 1))
+    setIsTimeStarted(true);
   };
 
   const wait = () => {
+    setIsTimeStarted(false);
   };
 
   const reset = () => {
+    if (time >= 1) {
+      setTime(0);
+      setIsTimeStarted(true);
+    }
   };
 
   const stop = () => {
-    setIsTimerStarted(false);
-    stream$.unsubscribe();
-  };
-
-  const startCount = () => {
-    
-  };
-
-  useEffect(() => {
-   
-  }, [isTimerStarted]);
+    setIsTimeStarted(false);
+    setTime(0)
+  }
 
   return (
     <div className="stopwatch">
-      {console.log('render')}
-
       <div className="stopwatch__screen">
+
         <div className="stopwatch__item">
-          00
+          {(`0${Math.floor((time / 3600) % 100)}`).slice(-2)}
         </div>
         <div className="stopwatch__border">:</div>
+
         <div className="stopwatch__item">
-          00
+          {(`0${Math.floor((time / 60) % 60)}`).slice(-2)}
         </div>
         <div className="stopwatch__border">:</div>
+
         <div className="stopwatch__item">
-          {seconds}
+          {(`0${Math.floor(time % 60)}`).slice(-2)}
         </div>
+        
       </div>
 
       <div className="stopwatch__buttons">
@@ -179,14 +70,14 @@ const Stopwatch = () => {
           type="button"
           className="stopwatch__button"
           onClick={start}
-          disabled={isTimerStarted}
+          disabled={isTimeStarted}
         >
           Start
         </button>
         <button
           type="button"
           className="stopwatch__button"
-          onClick={startCount}
+          onDoubleClick={wait}
         >
           Wait
         </button>
